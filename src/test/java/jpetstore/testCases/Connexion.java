2217316f9d30e98893d02ea8b8ee1baf61fc7a9b
HNikolaos <nhubert@henix.fr>
2022-11-19 16:51:47 +0100

    commit corrections

--- src/test/java/jpetstore/testCases/Connexion.java
+++ src/test/java/jpetstore/testCases/Connexion.java
@@ -1,12 +1,14 @@
 package jpetstore.testCases;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -20,17 +22,13 @@ import jpetstore.pageObject.PageCatalogue;
 import jpetstore.pageObject.PageConnexion;
 
 public class Connexion extends Nav {
-	
-	PageCatalogue page_catalogue = PageFactory.initElements(driver, PageCatalogue.class);
-	PageConnexion page_connexion = PageFactory.initElements(driver, PageConnexion.class);
-	
+		
 	@Given("un navigateur est ouvert")
 	public void un_navigateur_est_ouvert() throws Throwable {
 		System.setProperty("webdriver.gecko.driver", "./rsc/geckodriver.exe");
 		driver = new FirefoxDriver();
 //		System.setProperty("webdriver.chrome.driver", "./rsc/chromedriver.exe");
 //		driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 	}
 
 	@When("je suis sur url")
@@ -40,11 +38,14 @@ public class Connexion extends Nav {
 
 	@When("je clique sur le lien de connexion")
 	public void je_clique_sur_le_lien_de_connexion() throws Throwable {
+		PageCatalogue page_catalogue = PageFactory.initElements(driver, PageCatalogue.class);
 		page_catalogue.signIn.click();
 	}
-
+	
+	
 	@When("rentre le Username {string}")
 	public void rentre_le_Username(String username) throws Throwable {
+		PageConnexion page_connexion = PageFactory.initElements(driver, PageConnexion.class);
 		page_connexion.username.clear();
 		page_connexion.username.sendKeys(username);
 		
@@ -52,25 +53,26 @@ public class Connexion extends Nav {
 
 	@When("rentre le Password {string}")
 	public void rentre_le_Password(String password) throws Throwable {
+		PageConnexion page_connexion = PageFactory.initElements(driver, PageConnexion.class);
 		page_connexion.password.clear();
 		page_connexion.password.sendKeys(password);
 	}
 
 	@When("je clique sur login")
 	public void je_clique_sur_login() throws Throwable {
-		WebDriverWait wait = new WebDriverWait(driver, 2);
+		PageConnexion page_connexion = PageFactory.initElements(driver, PageConnexion.class);
 		page_connexion.login.click();
-//		WebElement signonButton = wait.until(ExpectedConditions.elementToBeClickable(By.name("signon")));
-//		signonButton.click();
 	}
 
 	@Then("utilisateur ABC est connecte")
 	public void utilisateur_ABC_est_connecte() throws Throwable {
+		PageCatalogue page_catalogue = PageFactory.initElements(driver, PageCatalogue.class);
 		assertEquals("Erreur sur la présence du bouton Sign Out","Sign Out",page_catalogue.signOut.getText());		
 	}
 
 	@Then("je peux lire le message accueil {string}")
 	public void je_peux_lire_le_message_accueil(String message) {
+		PageCatalogue page_catalogue = PageFactory.initElements(driver, PageCatalogue.class);
 		assertEquals("Message d'accueil erroné",message,page_catalogue.welcome.getText());
 	}
 	
